145c7540a573801b14df8206097fd2b292694442
aishcann <aish.cann@gmail.com>
2021-07-09 16:46:46 -0400

    Day 9 JS Fundamentals

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,61 @@
+//VALUES AND VARIABLES
+
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Jonas');
+console.log(23);
+
+// declaring a variable
+// type _ name = value of variable
+let firstName = "Jonas"
+
+// variables are a way to store data!
+// variables use camelCase
+// variable names should be descriptive! by reading the variable name, people should know what value the variable holds
+
+//descriptive variable names
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+//variable names that aren't too descriptive
+let job1 = "Programmer";
+let job2 = "Teacher";
+
+
+//console.log = outputs our code to the console
+
+console.log(firstName)
+
+//PRACTICE
+
+let country = "USA";
+let continent = "North America";
+let population = 328000000
+
+console.log(country, continent, population)
+
+//DATA TYPES
+
+//values are either an object or primitive value.
+
+//Primitive data types:
+//  Number = used for decimals and integers
+//  String = sequence of characters used for text, always in quotes
+//  Boolean = logical type that can only be true or false, used to take decisions
+//  Undefined = value taken by a varialbe that is not yet defined('empty value') ex: let children
+//  Null = also means 'empty value' but is used in different circumstances
+//  Symbol = defines a value that is unique and cannot be changed
+//  BigInt = large integars than what Number can hold
+
+//JS authomatically determines the value of a data type when it's stored in a variable (called dynamic typing)
+
+//Can find the type of value by console.logging with typeof
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof 'Jonas')
